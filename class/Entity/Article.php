cce1ee5a64fc89f30e68c6fd27b78dde375b0f95
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-11 14:49:36 +0200

    add doctrine mapping

--- class/Entity/Article.php
+++ class/Entity/Article.php
@@ -3,12 +3,44 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
+/**
+ * @Entity
+ * @Table(name="article")
+ */
 class Article 
-{
+{   
+    /**
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue
+     */
+    private $id;
+
+    /** 
+     * @Column(name="`title`", type="string") 
+     */
     private $title;
+
+    /** 
+     * @Column(name="`body`", type="text") 
+     */
     private $body;
+
+    /**
+     * One Student has One Mentor.
+     * @OneToOne(targetEntity="User")
+     * @JoinColumn(name="user_id", referencedColumnName="id")
+     */
     private $author;
+
+    /** 
+     * @Column(name="`created_date`", type="date") 
+     */
     private $created_date;
+
+    /** 
+     * @Column(name="`updated_date`", type="date") 
+     */
     private $updated_date;
     
 }
