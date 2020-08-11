e722822e0512b1ddf9904b71c133a87bb544107c
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-11 22:58:36 +0200

    add boostrap

--- template/articles.php
+++ template/articles.php
@@ -1,9 +1,15 @@
 <?php 
-    include './partials/header.php';
+    include 'partials/header.php';
 ?>
 
-    <h1>Articles title</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Articles</h1>
+            </div>
+        </div>
+    </div>
 
 <?php 
-    include './partials/footer.php';
+    include 'partials/footer.php';
 ?>
--- template/partials/footer.php
+++ template/partials/footer.php
@@ -1,2 +1,3 @@
+<script src="resources/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
