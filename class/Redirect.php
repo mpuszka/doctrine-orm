4e5a16e4a99eaeb0ad81e436d463fdb2bb299bef
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-14 13:55:56 +0200

    add comments to redirect classes

--- class/Redirect.php
+++ class/Redirect.php
@@ -3,12 +3,22 @@ declare(strict_types=1);
 
 namespace App;
 
+/**
+ * Redirect class
+ */
 class Redirect 
-{
+{   
+    /**
+     * Redirect method
+     *
+     * @param string $url
+     * @param integer $parameter
+     * @param boolean $permanent
+     * @return void
+     */
     public static function redirect(string $url, int $parameter = null, bool $permanent = false): void
     {
         header('Location: ' . $url . $parameter, true, $permanent ? 301 : 302);
-
         exit();
     }
 }
