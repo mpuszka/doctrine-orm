71c9e019a4dbe2c6948065410ef662fc958329f1
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-08-14 22:03:14 +0200

    remove obj from bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 include '../vendor/autoload.php';
 
 use App\Route;
-use App\DoctrineORM;
 
 $route  = new Route;
 $router = $route->getRouter();
@@ -12,5 +11,3 @@ $router = $route->getRouter();
 require 'config/routes.php';
 
 $route->runRouter();
-
-$doctrine = new DoctrineORM;
