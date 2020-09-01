9d38d675d4146daf51c492547d950c20eeef0f16
Marcin Puszka <marcin.puszka@iamdripfeed.com>
2020-09-01 11:45:03 +0200

    Remove unuseless commented lines

--- public/index.php
+++ public/index.php
@@ -2,13 +2,3 @@
 declare(strict_types=1);
 
 include '../bootstrap.php';
-
-// $router = new \Bramus\Router\Router();
-// $name = 'Marcin';
-// $router->get('/', function() use ($name) {
-//     echo 'About Page Contents' . $name;
-// });
-// $router->setNamespace('\App\Controllers');
-// $router->get('/', 'User@showProfile');
-
-// $router->run();
